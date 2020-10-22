fa452895dd8f1151b556105c0d1bfe987cd17905
boole.george.john@gmail.com <boole.george.john@gmail.com>
2020-10-22 17:20:09 +0100

    added hard drop

--- src/components/App.js
+++ src/components/App.js
@@ -62,6 +62,19 @@ class App extends Component {
             }
           }
           break;
+        case "Space":
+          if (this.props.paused === false && this.props.gameOver === false) {
+            e.preventDefault();
+
+            // clearDropInterval();
+            console.log("space");
+
+            while (!this.hitDetected()) {
+              this.props.moveDown();
+            }
+            this.props.moveUp();
+          }
+          break;
         case "ArrowUp":
         case "KeyE":
         case "Numpad8":
@@ -253,6 +266,77 @@ class App extends Component {
     );
   }
 
+  renderNextBlock() {
+    return mergeMatrices(
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+      this.props.activeBlock,
+      0,
+      0
+    ).map((row, index) => {
+      return (
+        <div key={index}>
+          {row.map((tile, index) => {
+            if (tile === 0) {
+              return <div className="empty-tile" key={index}></div>;
+            }
+            if (tile === 1) {
+              return (
+                <div className="filled-tile" key={index}>
+                  <div className="i block"></div>
+                </div>
+              );
+            }
+            if (tile === 2) {
+              return (
+                <div className="filled-tile" key={index}>
+                  <div className="j block"></div>
+                </div>
+              );
+            }
+            if (tile === 3) {
+              return (
+                <div className="filled-tile" key={index}>
+                  <div className="l block"></div>
+                </div>
+              );
+            }
+            if (tile === 4) {
+              return (
+                <div className="filled-tile" key={index}>
+                  <div className="o block"></div>
+                </div>
+              );
+            }
+            if (tile === 5) {
+              return (
+                <div className="filled-tile" key={index}>
+                  <div className="s block"></div>
+                </div>
+              );
+            }
+            if (tile === 6) {
+              return (
+                <div className="filled-tile" key={index}>
+                  <div className="t block"></div>
+                </div>
+              );
+            }
+            return (
+              <div className="filled-tile" key={index}>
+                <div className="z block"></div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    });
+  }
+
   renderField() {
     if (!this.props.paused) {
       return mergeMatrices(
@@ -351,6 +435,10 @@ class App extends Component {
             <h2 className="lines">
               LINES:
               <h2 className="lines num">{this.props.lines}</h2>
+            </h2>
+            <h2 className="next-block">
+              NEXT:
+              <div className="next-block preview">{this.renderNextBlock()}</div>
             </h2>
           </div>
           {this.props.gameOver && this.renderGameOver()}
