01c6d6b89f3ce951f9f13719a70d9c6bbb6f7cca
Graeme Richmond <richmond.graeme@gmail.com>
2021-03-02 02:54:37 +0000

    added display none to .score and .next-block

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 
-// import Score from "./Score";
+import Score from "./Score";
 import Level from "./Level";
 import Lines from "./Lines";
-// import NextBlock from "./NextBlock";
+import NextBlock from "./NextBlock";
 
 export class SidePanel extends Component {
   render() {
     return (
       <div className="side-panel">
-        {/* <Score /> */}
+        <Score />
         <Level />
+        <NextBlock />
         <Lines />
-        {/* <NextBlock /> */}
       </div>
     );
   }
